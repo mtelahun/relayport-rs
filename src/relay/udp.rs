891905d10e682bb274a86044d2af5037f3172e96
Michael Telahun <michael.telahun@dindin.et>
2023-12-12 14:15:39 +0300

    Udp: automatically close down relays after idle for 180 sec.

--- src/relay/udp.rs
+++ src/relay/udp.rs
@@ -3,11 +3,13 @@
 use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::time::Duration;
 
 use socket2::{Domain, Protocol, SockAddr, Type};
 use tokio::net::UdpSocket;
 use tokio::sync::broadcast::Receiver;
 use tokio::sync::RwLock;
+use tokio::time::sleep;
 use tracing::debug;
 
 use crate::command::RelayCommand;
@@ -15,6 +17,9 @@ use crate::RelayPortError;
 
 /// Maximum UDP buffer size
 const MAX_PKT_SIZE: usize = 10240;
+
+/// Maximum unidirectional link idle time
+const UDP_TTL: Duration = Duration::from_secs(180);
 
 /// Abstraction for initiating the relay builder.
 #[derive(Debug)]
@@ -28,8 +33,8 @@ pub struct RelaySocketBuilder(RelayInner);
 #[derive(Debug)]
 pub struct BoundRelaySocket {
     socket: InnerUdpSocket,
-    client_map: Arc<RwLock<HashMap<SocketAddr, InnerUdpSocket>>>,
-    peer_map_by_client_addr: Arc<RwLock<HashMap<SocketAddr, InnerUdpSocket>>>,
+    client_connections: Arc<RwLock<HashMap<SocketAddr, InnerUdpSocket>>>,
+    remote_connections: Arc<RwLock<HashMap<SocketAddr, InnerUdpSocket>>>,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -95,10 +100,8 @@ impl RelaySocketBuilder {
 
         Ok(BoundRelaySocket {
             socket: InnerUdpSocket::new(socket),
-            client_map: Arc::new(RwLock::new(HashMap::<SocketAddr, InnerUdpSocket>::new())),
-            peer_map_by_client_addr: Arc::new(RwLock::new(
-                HashMap::<SocketAddr, InnerUdpSocket>::new(),
-            )),
+            client_connections: Arc::new(RwLock::new(HashMap::<SocketAddr, InnerUdpSocket>::new())),
+            remote_connections: Arc::new(RwLock::new(HashMap::<SocketAddr, InnerUdpSocket>::new())),
         })
     }
 
@@ -173,9 +176,6 @@ impl BoundRelaySocket {
     /// This method will not return unless a [RelayCommand::Shutdown] is sent by the caller or
     /// it encounters an error. If it encounters an error it will return a RelayPortError.
     ///
-    /// # panics
-    /// This method may panic if it is unable to create a connection to the remote peer.
-    ///
     /// # Example
     /// ```no_run
     /// # use std::error::Error;
@@ -213,23 +213,41 @@ impl BoundRelaySocket {
     pub async fn serve(
         &self,
         remote: &str,
-        cancel: &Receiver<RelayCommand>,
+        command: &Receiver<RelayCommand>,
     ) -> Result<(), RelayPortError> {
         let remote_addr = remote.parse().map_err(RelayPortError::ParseAddrError)?;
         let my_addr = self.socket.0.local_addr()?;
+
+        let (cancel_tx, cancel_rx) = tokio::sync::mpsc::channel(24);
+        self.spawn_connection_cancel(cancel_rx).await;
         loop {
-            let mut cancel = cancel.resubscribe();
+            let mut command = command.resubscribe();
             let mut buf = [0u8; MAX_PKT_SIZE];
             let (len, client_addr) = self.socket.0.recv_from(&mut buf).await?;
-            debug!("received data from {}", client_addr);
+
+            // There is a delay between when we receive a packet and when we setup the bidirectional
+            // link (creating a race-condition). Catch any packets that arrive during this race.
+            if let Some(sock) = self.remote_connections.read().await.get(&client_addr) {
+                sock.0.send(&buf[0..len]).await?;
+                continue;
+            } else if let Some(sock) = self.client_connections.read().await.get(&client_addr) {
+                sock.0.send(&buf[0..len]).await?;
+                continue;
+            }
+
+            // This is a new connection. Send the initial packet to the remote and
+            // setup the bidirectional link.
+            let client_socket = self
+                .new_client_connection(&client_addr, Some(&my_addr))
+                .await?;
             let remote_socket = self
-                .get_remote_socket_by_client(&remote_addr, &client_addr)
-                .await;
-            let client_socket = self.get_client_socket(&client_addr, &my_addr).await;
+                .new_remote_connection(&remote_addr, &client_addr)
+                .await?;
+            let _ = remote_socket.0.send(&buf[0..len]).await?;
             tokio::select! {
                 biased;
-                _ = self.spawn_bidirectional_relay(&client_socket, &remote_socket, cancel.resubscribe(), buf, len) => { continue }
-                result = cancel.recv() => {match result {
+                _ = self.spawn_bidirectional_relay(client_socket, remote_socket, command.resubscribe(), cancel_tx.clone()) => { continue }
+                result = command.recv() => {match result {
                     Ok(cmd) => match cmd {
                         RelayCommand::Shutdown => {
                             debug!("received relay command: shutdown");
@@ -244,67 +262,30 @@ impl BoundRelaySocket {
         Ok(())
     }
 
-    /// Obtain a pre-existing socket to a remote peer or create a new one.
+    /// Connects a UDP socket to the remote peer, denoted by addr, to which we are relaying traffic.
+    /// The addr argument must be a string slice that can be converted to a [`SocketAddr`]
+    /// object. The bind_addr argument is an Option containing a string slice that specifies the
+    /// local address the socket should bind to. If it is None the method will bind
+    /// to a random local address.
+    /// # Returns
+    /// On success the unit type () is returned. Otherwise a RelayPortError error is returned.
     ///
-    /// If it has *NOT* already seen the remote [addr] connected to from [client_addr] it will
-    /// connect a new UDP socket to [addr], indexed by [client_addr], and add it to its global
-    /// list of sockets.
-    async fn get_remote_socket_by_client(
+    async fn new_remote_connection(
         &self,
         addr: &SocketAddr,
         client_addr: &SocketAddr,
-    ) -> InnerUdpSocket {
-        {
-            let peer_map = self.peer_map_by_client_addr.read().await;
-            if let Some(peer) = peer_map.get(client_addr) {
-                return peer.clone();
-            }
+    ) -> Result<InnerUdpSocket, RelayPortError> {
+        let mut peer_map_by_client_addr = self.remote_connections.write().await;
+        if !peer_map_by_client_addr.contains_key(client_addr) {
+            let udpsock = UdpSocket::bind("0.0.0.0:0").await?;
+            udpsock.connect(addr).await?;
+            let udpsock = InnerUdpSocket::new(udpsock);
+            peer_map_by_client_addr.insert(*client_addr, udpsock.clone());
+
+            return Ok(udpsock);
         }
 
-        self.new_inner_remote_socket(addr, client_addr)
-            .await
-            .unwrap_or_else(|_| {
-                panic!("failed to connect to remote peer on behalf of {client_addr}")
-            })
-    }
-
-    async fn get_client_socket(&self, addr: &SocketAddr, bind_addr: &SocketAddr) -> InnerUdpSocket {
-        {
-            let client_map = self.client_map.read().await;
-            if let Some(client) = client_map.get(addr) {
-                return client.clone();
-            }
-        }
-
-        self.new_inner_client_socket(addr, Some(bind_addr))
-            .await
-            .unwrap_or_else(|_| panic!("failed to bind to {bind_addr} on behalf of client {addr}"))
-    }
-
-    async fn spawn_bidirectional_relay(
-        &self,
-        client: &InnerUdpSocket,
-        remote: &InnerUdpSocket,
-        cancel: Receiver<RelayCommand>,
-        buf: [u8; MAX_PKT_SIZE],
-        len: usize,
-    ) -> Result<(), RelayPortError> {
-        let client = client.clone();
-        let remote = remote.clone();
-        let empty_buf = [0u8; MAX_PKT_SIZE];
-
-        tokio::spawn(async move {
-            let client_r = client.clone();
-            let client_w = client.clone();
-            let remote_r = remote.clone();
-            let remote_w = remote.clone();
-            let _ = tokio::join!(
-                single_direction_relay(&client_r, &remote_w, cancel.resubscribe(), &buf, len),
-                single_direction_relay(&remote_r, &client_w, cancel, &empty_buf, 0),
-            );
-        });
-
-        Ok(())
+        Ok(peer_map_by_client_addr.get(client_addr).unwrap().clone())
     }
 
     /// Connects a UDP socket to a client peer denoted by addr. The addr
@@ -315,12 +296,12 @@ impl BoundRelaySocket {
     /// # Returns
     /// On success the unit type () is returned. Otherwise a RelayPortError error is returned.
     ///
-    async fn new_inner_client_socket(
+    async fn new_client_connection(
         &self,
         addr: &SocketAddr,
         bind_addr: Option<&SocketAddr>,
     ) -> Result<InnerUdpSocket, RelayPortError> {
-        let mut client_map = self.client_map.write().await;
+        let mut client_map = self.client_connections.write().await;
         if !client_map.contains_key(addr) {
             let socket;
             if let Some(local_addr) = bind_addr {
@@ -345,30 +326,58 @@ impl BoundRelaySocket {
         Ok(client_map.get(addr).unwrap().clone())
     }
 
-    /// Connects a UDP socket to the remote peer, denoted by addr, to which we are relaying traffic.
-    /// The addr argument must be a string slice that can be converted to a [`SocketAddr`]
-    /// object. The bind_addr argument is an Option containing a string slice that specifies the
-    /// local address the socket should bind to. If it is None the method will bind
-    /// to a random local address.
-    /// # Returns
-    /// On success the unit type () is returned. Otherwise a RelayPortError error is returned.
-    ///
-    async fn new_inner_remote_socket(
+    async fn spawn_bidirectional_relay(
         &self,
-        addr: &SocketAddr,
-        client_addr: &SocketAddr,
-    ) -> Result<InnerUdpSocket, RelayPortError> {
-        let mut peer_map_by_client_addr = self.peer_map_by_client_addr.write().await;
-        if !peer_map_by_client_addr.contains_key(client_addr) {
-            let udpsock = UdpSocket::bind("0.0.0.0:0").await?;
-            udpsock.connect(addr).await?;
-            let udpsock = InnerUdpSocket::new(udpsock);
-            peer_map_by_client_addr.insert(*client_addr, udpsock.clone());
+        client: InnerUdpSocket,
+        remote: InnerUdpSocket,
+        command: Receiver<RelayCommand>,
+        cancel: tokio::sync::mpsc::Sender<InnerUdpSocket>,
+    ) {
+        let client = client.clone();
+        let remote = remote.clone();
 
-            return Ok(udpsock);
-        }
+        tokio::spawn(async move {
+            let _ = tokio::join!(
+                unidirectional_relay(
+                    client.clone(),
+                    remote.clone(),
+                    command.resubscribe(),
+                    cancel.clone()
+                ),
+                unidirectional_relay(remote.clone(), client.clone(), command, cancel),
+            );
+        });
+    }
 
-        Ok(peer_map_by_client_addr.get(client_addr).unwrap().clone())
+    /// Remove an entry each from the global client and remote connection lists.
+    ///
+    /// When a [tokio::sync::mpsc] sender sends a [tokio::sync::mpsc::Receiver] message containing
+    /// a socket this method will obtain the associated remote address and use it to
+    /// remove the associated socket from our list of client and remote connections.
+    async fn spawn_connection_cancel(
+        &self,
+        mut socket: tokio::sync::mpsc::Receiver<InnerUdpSocket>,
+    ) {
+        let c_connections = self.client_connections.clone();
+        let r_connections = self.remote_connections.clone();
+        tokio::task::spawn(async move {
+            while let Some(socket) = socket.recv().await {
+                let mut connections = c_connections.write().await;
+                if let Ok(peer_addr) = socket.0.peer_addr() {
+                    debug!("cancel connection with: {peer_addr}");
+                    if connections.contains_key(&peer_addr) {
+                        connections.remove(&peer_addr);
+                    }
+                }
+
+                let mut connections = r_connections.write().await;
+                if let Ok(peer_addr) = socket.0.peer_addr() {
+                    if connections.contains_key(&peer_addr) {
+                        connections.remove(&peer_addr);
+                    }
+                }
+            }
+        });
     }
 }
 
@@ -379,57 +388,65 @@ impl InnerUdpSocket {
 }
 
 #[tracing::instrument(level = "debug", skip_all, err, ret, fields(from_addr, to_addr))]
-async fn single_direction_relay(
-    src: &InnerUdpSocket,
-    dst: &InnerUdpSocket,
-    mut rx: Receiver<RelayCommand>,
-    buf: &[u8],
-    len: usize,
+async fn unidirectional_relay(
+    src: InnerUdpSocket,
+    dst: InnerUdpSocket,
+    mut command: Receiver<RelayCommand>,
+    cancel: tokio::sync::mpsc::Sender<InnerUdpSocket>,
 ) -> Result<usize, RelayPortError> {
     let from_addr = src.0.peer_addr().unwrap();
     let to_addr = dst.0.peer_addr().unwrap();
-    if len != 0 {
-        let _ = dst.0.send(&buf[0..len]).await?;
-        debug!("wrote initial udp packet: {from_addr} -> {to_addr}");
-    }
 
     let mut xfer_bytes = 0;
     let mut buf = [0u8; MAX_PKT_SIZE];
+    let src_ = src.clone();
+    let dst_ = dst.clone();
     let copy_reader_to_writer = async move {
-        while let Ok(len) = src.0.recv(&mut buf).await {
-            debug!("received {len} bytes from {from_addr}");
+        while let Ok(len) = src_.0.recv(&mut buf).await {
             if len == 0 {
                 break;
             }
-            let result = dst.0.send(&buf[0..len]).await;
+            let result = dst_.0.send(&buf[0..len]).await;
             if result.is_err() {
                 let e = result.err().unwrap();
-                eprintln!("failed to send packet to {to_addr}: {e}");
+                eprintln!("failed to relay packet: {from_addr} -> {to_addr}: {e}");
                 return Err(e);
             } else {
                 let len = result.ok().unwrap();
-                debug!("wrote {len} bytes to {to_addr}");
                 xfer_bytes += len;
             }
         }
 
         Ok(xfer_bytes)
     };
-    let await_shutdown = rx.recv();
+    let await_shutdown = command.recv();
     let mut read_bytes = Ok(0);
     tokio::select! {
         biased;
-            result = copy_reader_to_writer => {
-                read_bytes = result
-            }
-            result = await_shutdown => { match result {
-                Ok(cmd) => match cmd {
-                    RelayCommand::Shutdown => {
-                        debug!("received shutdown relay command");
-                    },
+        result = copy_reader_to_writer => {
+            read_bytes = result
+        }
+        result = await_shutdown => { match result {
+            Ok(cmd) => match cmd {
+                RelayCommand::Shutdown => {
+                    debug!("received shutdown relay command");
                 },
-                Err(e) => return Err(RelayPortError::InternalCommunicationError(e)),
-            }}
+            },
+            Err(e) => return Err(RelayPortError::InternalCommunicationError(e)),
+        }}
+        _ = sleep(UDP_TTL) => {
+            // The peer address from either src or dst will match to enable us to
+            // remove the socket from the client/remote connections lists. So, send the
+            // cancel command using both.
+            cancel
+                .send(src)
+                .await
+                .map_err(|e| RelayPortError::Unknown(format!("{e}")))?;
+            cancel
+                .send(dst)
+                .await
+                .map_err(|e| RelayPortError::Unknown(format!("{e}")))?;
+        }
     }
 
     match read_bytes {
